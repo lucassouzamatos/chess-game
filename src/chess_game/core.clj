c76ccd95de7fd81bcedaa92272e11342c0d55e03
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-10-29 21:34:06 -0300

    refactor threading to doto

--- src/chess_game/core.clj
+++ src/chess_game/core.clj
@@ -6,6 +6,6 @@
   "I don't do a whole lot ... yet."
   [& args]
   (let [board (board/make-board)]
-    (-> board
+    (doto board
      (board/move-piece ,,, {:x 1 :y 1} {:x 1 :y 2})
      (board/move-piece ,,, {:x 1 :y 2} {:x 1 :y 3}))))
